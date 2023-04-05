6aa0a6babbffd0a54ba0f1f97ef5a98d5ae3d693
Danindu Marasinghe <dmarasin@uoguelph.ca>
2023-04-05 13:04:39 -0400

    function stub for btnInsMol

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 /* javascript to accompany jquery.html */
 
 $(document).ready(
-
+   /* this defines a function that gets called after the document is in memory */
+  function()
+  {
+      // click handler for uploadSDF button
+      $("#btnInsMol").click(
+         function()
+         {
+            
+         }
+      )
+  }
 );
