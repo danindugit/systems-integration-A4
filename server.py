7097ed76a399ff2a466b8f39b83ac3957ff8aff4
Danindu Marasinghe <dmarasin@uoguelph.ca>
2023-04-05 18:02:36 -0400

    write_file

--- server.py
+++ server.py
@@ -10,101 +10,114 @@ import molsql;
 public_files = [ '/index.html', '/style.css', '/script.js' , '/addElement.html', '/removeElement.html', '/uploadSDF.html', '/selectMolecule.html'];
 
 class MyHandler( BaseHTTPRequestHandler ):
-    def do_GET(self):
+   def do_GET(self):
 
-        # used to GET a file from the list ov public_files, above
-         if self.path == "/":   # make sure it's a valid file
-            self.send_response( 200 );  # OK
-            self.send_header( "Content-type", "text/html" );
+      # used to GET a file from the list ov public_files, above
+      if self.path == "/":   # make sure it's a valid file
+         self.send_response( 200 );  # OK
+         self.send_header( "Content-type", "text/html" );
 
-            self.write_file("index.html");
+         self.write_file("index.html");
 
-         elif self.path == "/style.css":
-            self.send_response(200); # ok
-            self.send_header( "Content-type", "text/css" );   
+      elif self.path == "/style.css":
+         self.send_response(200); # ok
+         self.send_header( "Content-type", "text/css" );   
 
-            self.write_file( "style.css" );
+         self.write_file( "style.css" );
 
-         elif self.path == "/addElement.html":
-            self.send_response(200); # ok
-            self.send_header( "Content-type", "text/html" );   
+      elif self.path == "/addElement.html":
+         self.send_response(200); # ok
+         self.send_header( "Content-type", "text/html" );   
 
-            self.write_file( "addElement.html" );
+         self.write_file( "addElement.html" );
 
-         elif self.path == "/removeElement.html":
-            self.send_response(200); # ok
-            self.send_header( "Content-type", "text/html" );   
+      elif self.path == "/removeElement.html":
+         self.send_response(200); # ok
+         self.send_header( "Content-type", "text/html" );   
 
-            self.write_file( "removeElement.html" );
-         
-         elif self.path == "/selectMolecule.html":
-            self.send_response(200); # ok
-            self.send_header( "Content-type", "text/html" );   
+         self.write_file( "removeElement.html" );
+      
+      elif self.path == "/selectMolecule.html":
+         self.send_response(200); # ok
+         self.send_header( "Content-type", "text/html" );   
 
-            self.write_file( "selectMolecule.html" );
-         
-         elif self.path == "/uploadSDF.html":
-            self.send_response(200); # ok
-            self.send_header( "Content-type", "text/html" );   
+         self.write_file( "selectMolecule.html" );
+      
+      elif self.path == "/uploadSDF.html":
+         self.send_response(200); # ok
+         self.send_header( "Content-type", "text/html" );   
 
-            self.write_file( "uploadSDF.html" );
+         self.write_file( "uploadSDF.html" );
 
-         else:
-            # if the requested URL is not one of the public_files
-            self.send_response( 404 );
-            self.end_headers();
-            self.wfile.write( bytes( "404: not found", "utf-8" ) );
-
-
-
-    def do_POST(self):
-
-        if self.path == "/sdf_upload.html":
-            # code to handle sdf_upload
-
-            molecule = MolDisplay.Molecule();
-            for i in range(0,4):    # skip 4 lines
-                string = next(self.rfile);
-
-            molsql.add_molecule( self.rfile );
-
-            message = "sdf file uploaded to database";
-
-            self.send_response( 200 ); # OK
-            self.send_header( "Content-type", "text/plain" );
-            self.send_header( "Content-length", len(message) );
-            self.end_headers();
-
-            self.wfile.write( bytes( message, "utf-8" ) );
-
-        elif self.path == "/form_handler.html":
-
-            # this is specific to 'multipart/form-data' encoding used by POST
-            content_length = int(self.headers['Content-Length']);
-            body = self.rfile.read(content_length);
-
-            print( repr( body.decode('utf-8') ) );
-
-            # convert POST content into a dictionary
-            postvars = urllib.parse.parse_qs( body.decode( 'utf-8' ) );
-
-            print( postvars );
-
-            message = "data received";
-
-            self.send_response( 200 ); # OK
-            self.send_header( "Content-type", "text/plain" );
-            self.send_header( "Content-length", len(message) );
-            self.end_headers();
-
-            self.wfile.write( bytes( message, "utf-8" ) );
-
-        else:
-            self.send_response( 404 );
-            self.end_headers();
-            self.wfile.write( bytes( "404: not found", "utf-8" ) );
+      else:
+         # if the requested URL is not one of the public_files
+         self.send_response( 404 );
+         self.end_headers();
+         self.wfile.write( bytes( "404: not found", "utf-8" ) );
 
 
+
+   def do_POST(self):
+
+      if self.path == "/sdf_upload.html":
+         # code to handle sdf_upload
+
+         molecule = MolDisplay.Molecule();
+         for i in range(0,4):    # skip 4 lines
+               string = next(self.rfile);
+
+         molsql.add_molecule( self.rfile );
+
+         message = "sdf file uploaded to database";
+
+         self.send_response( 200 ); # OK
+         self.send_header( "Content-type", "text/plain" );
+         self.send_header( "Content-length", len(message) );
+         self.end_headers();
+
+         self.wfile.write( bytes( message, "utf-8" ) );
+
+      elif self.path == "/form_handler.html":
+
+         # this is specific to 'multipart/form-data' encoding used by POST
+         content_length = int(self.headers['Content-Length']);
+         body = self.rfile.read(content_length);
+
+         print( repr( body.decode('utf-8') ) );
+
+         # convert POST content into a dictionary
+         postvars = urllib.parse.parse_qs( body.decode( 'utf-8' ) );
+
+         print( postvars );
+
+         message = "data received";
+
+         self.send_response( 200 ); # OK
+         self.send_header( "Content-type", "text/plain" );
+         self.send_header( "Content-length", len(message) );
+         self.end_headers();
+
+         self.wfile.write( bytes( message, "utf-8" ) );
+
+      else:
+         self.send_response( 404 );
+         self.end_headers();
+         self.wfile.write( bytes( "404: not found", "utf-8" ) );
+
+   def write_file(self, filename):
+      self.send_response(200); # ok
+      fp = open(filename);
+
+      # load the specified file
+      page = fp.read();
+      fp.close();
+
+      # create and send headers
+      self.send_header( "Content-length", len(page) );
+      self.end_headers();
+
+      # send the contents
+      self.wfile.write( bytes( page, "utf-8" ) );
 
 
 httpd = HTTPServer( ( 'localhost', int(sys.argv[1]) ), MyHandler );
